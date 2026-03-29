d905ac30e37cfb67c4d4fb81462ee5eb92fef7e1
Александр Арискин <alexunderkot@gmail.com>
2026-03-29 15:38:35 +0300

    faStr1

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,14 +1,61 @@
 // Copyright 2022 UNN-IASR
-#include "fun.h"
+#include "../include/fun.h"
+#include <map>
 
-unsigned int faStr1(const char *str) {
+// Разработать функцию на языке С++, которая в переданной строке определяет слова,
+// не содержащие цифр и подсчитывает количество таких слов.
+unsigned int faStr1(const char *str)
+{
+    std::map<char, int> numbers;
+    numbers.insert({{'0', 0},
+                    {'1', 1},
+                    {'2', 2},
+                    {'3', 3},
+                    {'4', 4},
+                    {'5', 5},
+                    {'6', 6},
+                    {'7', 7},
+                    {'8', 8},
+                    {'9', 9}});
+
+    const char *pointer = str;
+    char currentChar = *str;
+    int count = 0;
+    bool hasNums, isWord;
+
+    // слово началось или уже идёт(текущий не пробел или изСлово) -) нет цифр -) текущий стал пробелом -) выход
+    while (true)
+    {
+        hasNums = false;
+        isWord = false;
+        while (currentChar != '\0' && currentChar != ' ' && currentChar != '\t' && currentChar != '\n') // пока мы в слове
+        {
+            isWord = true;
+            if (numbers.count(currentChar))
+                hasNums = true;
+            pointer++;
+            currentChar = *pointer;
+        }
+        if (!hasNums && isWord)
+            count++;
+        if (currentChar == '\0')
+            break;
+        pointer++;
+        currentChar = *pointer;
+    }
+    return count;
+}
+
+// Разработать функцию на языке С++, которая в переданной строке подсчитывает количество слов,
+// начинающиеся на заглавную латинскую букву, а среди других символов этих слов только латинские строчные буквы.
+unsigned int faStr2(const char *str)
+{
     return 0;
 }
 
-unsigned int faStr2(const char *str) {
-    return 0;
-}
-
-unsigned int faStr3(const char *str) {
+// Разработать функцию на языке С++, которая находит среднюю длину слова в строке,
+// округляя ее до целого значения по правилам округления.
+unsigned int faStr3(const char *str)
+{
     return 0;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,22 @@
 // Copyright 2022 UNN-IASR
-#include "fun.h"
+/*В заголовочный файл fun.h необходимо поместить заголовоки функции.
+В файл с исходным кодом fun.cpp помещаются реализации функций.
+Файл main.cpp содержит пример использования функций.
+Структура проекта
+include/fun.h - заголовочный файл с заголовком функции.
+src/fun.cpp - файл с реализацией функции.
+src/main.cpp - демонстрационная программа.*/
+#include "../include/fun.h"
+#include "fun.cpp"
+#include <stdio.h>
 
-int main() {
-    return 0;
+int main()
+{
+    printf("%d\t",faStr1(""));                         // 0
+    printf("%d\t",faStr1("hello"));                    // 1
+    printf("%d\t",faStr1("hello world"));              // 2
+    printf("%d\t",faStr1("hello123 world"));           // 1
+    printf("%d\t",faStr1("123 456"));                  // 0
+    printf("%d\t",faStr1("  hello   world  "));        // 2
+    printf("%d\t",faStr1("hello\tworld\nfoo123 bar")); // 3
 }
